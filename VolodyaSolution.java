76faae1d00588db931cfa50350b2cfd64c6e39b4
Volodymyr Zhabiuk <vzhabiuk@gmail.com>
2017-06-26 08:29:31 -0700

    Update VolodyaSolution.java

--- VolodyaSolution.java
+++ VolodyaSolution.java
@@ -1,50 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class VolodyaSolution {
 
-private static List<String> readWords(InputStream inputStream) {
-  try (InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
-    return new BufferedReader(reader).lines().filter(line -> !line.isEmpty()).collect(Collectors.toList());
-  }
-}
-  
-private static String findMaxSequenceMain(List<String> words) {
-  Map<Char, List<String>> wordsByStartLetter = new HashMap<>();
-  //instead of this for loop we could do words.stream().collect(Collectors.groupingBy(word -> word.charAt(0)));
-  for (String word : words) {
-    Char firstChar = word.charAt(0);
-    wordsByStartLetter.computeIfAbsent(firstChar, key -> new ArrayList<>()).add(word);
-  }
-  Set<String> visitedWords = new HashSet<>();
-  String currentMax = "";
-  for (Char firstLetter : wordsByStartLetter.keySet()) {
-    currentSequence.append(word);
-    String newMax = findMaxSequenceRecursive(wordsByStartLetter, visitedWords, firstLetter);
-    if (newMax.length() > currentMax.length()) {
-       currentMax = newMax;
-    }
-  }
-  return currentMax;
-} 
-  //Poshyk v glubuny na grafi
-  private static String findMaxSequenceRecursive(Map<Char, List<String>> wordsByStartLetter, 
-                                               Set<String> visitedWords, 
-                                               Char firstLetter) {
-    String currentMax = "";
-    for (String word : wordsByStartLetter.get(firstLetter)) {
-      if (!visitedWords.contains(word)) {
-        visitedWords.add(word);
-        Char lastWordChar = word.charAt(word.length() - 1);
-        String tail = findMaxSequenceRecursive(wordsByStartLetter, visitedWords, lastWordChar);
-        if (currenMax.length() < tail.length() + word.length()) {
-           currentMax = word + tail;
+    private static List<String> readWords(InputStream inputStream) {
+
+        try (InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
+            return new BufferedReader(reader).lines().filter(line -> !line.isEmpty()).collect(Collectors.toList());
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
         }
-        visitedWords.remove(word);
-      }
     }
-    return currenMax;
-  }
-  
-  public static void main(String[] args) {
-    System.out.println("Max sequence = " + findMaxSequenceMain(readWords(System.in)));
-  }
+
+    private static String findMaxSequenceMain(List<String> words) {
+        Map<Character, List<String>> wordsByStartLetter = new HashMap<>();
+        //instead of this for loop we could do words.stream().collect(Collectors.groupingBy(word -> word.charAt(0)));
+        for (String word : words) {
+            Character firstChar = word.charAt(0);
+            wordsByStartLetter.computeIfAbsent(firstChar, key -> new ArrayList<>()).add(word);
+        }
+        Set<String> visitedWords = new HashSet<>();
+        String currentMax = "";
+        for (Character firstLetter : wordsByStartLetter.keySet()) {
+            String newMax = findMaxSequenceRecursive(wordsByStartLetter, visitedWords, firstLetter);
+            if (newMax.length() > currentMax.length()) {
+                currentMax = newMax;
+            }
+        }
+        return currentMax;
+    }
+    //Poshyk v glubuny na grafi
+    private static String findMaxSequenceRecursive(Map<Character, List<String>> wordsByStartLetter,
+                                                   Set<String> visitedWords,
+                                                   Character firstLetter) {
+        String currentMax = "";
+        for (String word : wordsByStartLetter.get(firstLetter)) {
+            if (!visitedWords.contains(word)) {
+                visitedWords.add(word);
+                Character lastWordChar = word.charAt(word.length() - 1);
+                String tail = findMaxSequenceRecursive(wordsByStartLetter, visitedWords, lastWordChar);
+                if (currentMax.length() < tail.length() + word.length()) {
+                    currentMax = word + tail;
+                }
+                visitedWords.remove(word);
+            }
+        }
+        return currentMax;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Max sequence = " + findMaxSequenceMain(readWords(System.in)));
+    }
 }
